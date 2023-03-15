f11d1c2cc3eda222f3f6de9935b9da52a076db63
KapNelson <kapitannelson.wf@gmail.com>
2023-03-15 20:12:16 +0200

    init 69

---
+++ DockerJavaService/src/main/java/ukr/nure/itm/inf/dockerjavaservice/DockerJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package ukr.nure.itm.inf.dockerjavaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJavaServiceApplication.class, args);
+	}
+
+}
---
+++ DockerJavaService/src/test/java/ukr/nure/itm/inf/dockerjavaservice/DockerJavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ukr.nure.itm.inf.dockerjavaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

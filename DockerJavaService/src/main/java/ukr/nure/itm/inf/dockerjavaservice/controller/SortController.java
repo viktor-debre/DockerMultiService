479960237f20f7a045a42eb493db6ac85e901b78
KapNelson <kapitannelson.wf@gmail.com>
2023-03-18 14:15:45 +0200

    Add bubble sort for Java Service

---
+++ DockerJavaService/src/main/java/ukr/nure/itm/inf/dockerjavaservice/controller/SortController.java
@@ -0,0 +1,28 @@
+package ukr.nure.itm.inf.dockerjavaservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class SortController {
+
+    @PostMapping("/bubbleSort")
+    @ResponseBody
+    public List<Integer> bubbleSort(@RequestBody final List<Integer> array) {
+        int n = array.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array.get(j) > array.get(j + 1)) {
+                    int temp = array.get(j);
+                    array.set(j, array.get(j + 1));
+                    array.set(j + 1, temp);
+                }
+            }
+        }
+        return array;
+    }
+}
